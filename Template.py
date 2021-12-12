b6c229d603eb15aaa492b149efeeb34b686cd061
AntWort21 <67149821+AntWort21@users.noreply.github.com>
2021-12-12 20:47:57 +0700

    added comments

--- Template.py
+++ Template.py
@@ -6,9 +6,23 @@ from matplotlib import pyplot as plt
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def get_train_image(path):
+    '''
+        To get a list of train images, images label, and images index using the given path
 
-    #path = 'Dataset/Train'
-
+        Parameters
+        ----------
+        path : str
+            Location of train root directory
+        
+        Returns
+        -------
+        list
+            List containing all the train images
+        list
+            List containing all train images label
+        list
+            List containing all train images indexes
+    '''
     face_list = []
     face_list_id = []
     face_dir_list = os.listdir(path)
@@ -24,7 +38,19 @@ def get_train_image(path):
 
 
 def get_all_test_folders(path):
-    #path = 'Dataset/Test'
+    '''
+        To get a list of test subdirectories using the given path
+
+        Parameters
+        ----------
+        path : str
+            Location of test root directory
+        
+        Returns
+        -------
+        list
+            List containing all the test subdirectories
+    '''
     directories = []
 
     for i in os.listdir(path):
@@ -34,8 +60,20 @@ def get_all_test_folders(path):
 
 
 def get_all_test_images(path):
+    '''
+        To load a list of test images from given path list. Resize image height 
+        to 200 pixels and image width to the corresponding ratio for train images
 
-
+        Parameters
+        ----------
+        path : str
+            Location of images root directory
+        
+        Returns
+        -------
+        list
+            List containing all image that has been resized for each Test Folders
+    '''
     #path = 'Dataset/Test/Room 1'
     resized_img_list = []
 
@@ -50,16 +88,29 @@ def get_all_test_images(path):
 
 
 def detect_faces_and_filter(faces_list, labels_list=None):
-    #grayed_trained_images_list, _, grayed_trained_labels_list = detect_faces_and_filter(faces_list, indexes_list)
-    # return face_list, face_list_id
-    # face_list -> each person image
-    #face list_id -> index of the person
+    '''
+        To detect a face from given image list and filter it if the face on
+        the given image is not equals to one
 
+        Parameters
+        ----------
+        faces_list : list
+            List containing all loaded images
+        labels_list : list
+            List containing all image classes labels
+        
+        Returns
+        -------
+        list
+            List containing all filtered and cropped face images in grayscale
+        list
+            list containing image gray face location
+        list
+            List containing all filtered image classes label
+    '''
     face_list_gray = []
     label_list_gray = []
     image_gray_location = []
-
-    # print(labels_list)
 
     if(labels_list != None): #Training
         for (idx, img) in zip(labels_list, faces_list):
@@ -89,11 +140,22 @@ def detect_faces_and_filter(faces_list, labels_list=None):
     return face_list_gray, image_gray_location, label_list_gray  
 
 def train(grayed_images_list, grayed_labels_list):
+    '''
+        To create and train face recognizer object
 
+        Parameters
+        ----------
+        grayed_images_list : list
+            List containing all filtered and cropped face images in grayscale
+        grayed_labels : list
+            List containing all filtered image classes label
+        
+        Returns
+        -------
+        object
+            Recognizer object after being trained with cropped face images
+    '''
     face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-    # face_recognizer = cv2.face.EigenFaceRecognizer_create()
-    # face_recognizer = cv2.face.FisherFaceRecognizer_create()
-
     face_recognizer.train(grayed_images_list, np.array(grayed_labels_list))
 
     return face_recognizer
@@ -128,6 +190,21 @@ def predict(recognizer, gray_test_image_list):
 
 
 def check_attandee(predicted_name, room_number):
+    '''
+        To check the predicted user is in the designed room or not
+
+        Parameters
+        ----------
+        predicted_name : str
+            The name result from predicted user
+        room_number : int
+            The room number that the predicted user entered
+
+        Returns
+        -------
+        bool
+            If predicted user entered the correct room return True otherwise False
+    '''
     room1participant = [
         'Elon Musk',
         'Steve Jobs',
@@ -152,6 +229,28 @@ def check_attandee(predicted_name, room_number):
 
     
 def write_prediction(predict_results, test_image_list, test_faces_rects, train_names, room):
+    '''
+        To draw prediction and validation results on the given test images
+
+        Parameters
+        ----------
+        predict_results : list
+            List containing all prediction results from given test faces
+        test_image_list : list
+            List containing all loaded test images
+        test_faces_rects : list
+            List containing all filtered faces location saved in rectangle
+        train_names : list
+            List containing the names of the train sub-directories
+        room: int
+            The room number
+
+        Returns
+        -------
+        list
+            List containing all test images after being drawn with
+            its prediction and validation results
+    '''
     predicted_test_image_list = []
 
     for image, face_rect, [res, conf] in zip(test_image_list, test_faces_rects, predict_results):
@@ -171,6 +270,16 @@ def write_prediction(predict_results, test_image_list, test_faces_rects, train_n
 
 
 def combine_and_show_result(room, predicted_test_image_list):
+    '''
+        To show the final image that already combine into one image
+
+        Parameters
+        ----------
+        room : str
+            The room number in string format (e.g. 'Room 1')
+        predicted_test_image_list : nparray
+            Array containing image data
+    '''
     image_merge = []
     for image in predicted_test_image_list:
         ratio = 200/image.shape[0]
@@ -238,6 +347,7 @@ def main():
     '''
 
     test_images_folder = get_all_test_folders(test_path)
+
     for index, room in enumerate(test_images_folder):
         test_images_list = get_all_test_images(test_path + '/' + room)
         grayed_test_image_list, grayed_test_location, _ = detect_faces_and_filter(test_images_list)
